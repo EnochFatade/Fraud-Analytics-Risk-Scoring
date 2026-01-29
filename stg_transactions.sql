67bbaf596d4a77e0b42c9bc049d0679e17bc9292
EnochFatade <mej116@txstate.edu>
2026-01-29 09:41:18 -0600

    sql/10_staging/stg_transactions.sql
    sql: document staging

--- stg_transactions.sql
+++ stg_transactions.sql
@@ -1,14 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 -- Purpose:\
--- Define fraud rule logic and severity classification.\
--- Rules are evaluated at the transaction level and stored\
--- in the fraud_rule_hits fact table.\
---\
--- Severity thresholds (HIGH / MED) are configured in Rule_Config.\
+-- Purpose:
+-- Define fraud rule logic and severity classification.
+-- Rules are evaluated at the transaction level and stored
+-- in the fraud_rule_hits fact table.
+-- Severity thresholds (HIGH / MED) are configured in Rule_Config.
 }
